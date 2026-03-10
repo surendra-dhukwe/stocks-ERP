428fd0ac4a0a6037b4d452fe7c7002c8dda2e587
Surendra Dhukwe <surendradhukwe1234@gmail.com>
2026-03-10 22:49:53 +0800

    updated .env

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,13 @@
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-
-host:'localhost',
-user:'root',
-password:'surendra@123',
-database:'stock',
-
-waitForConnections:true,
-connectionLimit:10
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT
 });
 
-module.exports = db.promise();
+module.exports = db;
